0e02ce3a69ba20557fb5ca1ce8b02a288f0b212f
clifford <kpoku823@gmail.com>
2026-02-13 18:44:59 +0000

    tf update

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraform823" {
+  bucket = "terraform823"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform823"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
